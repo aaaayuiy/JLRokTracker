16b298e02bfa03cf9f56646987d1c7f44a7c3239
Jiaxin Li <jiaxin.li1992@gmail.com>
2023-11-02 21:39:47 +0000

    Update roktracker.bat

--- roktracker.bat
+++ roktracker.bat
@@ -4,9 +4,9 @@ set /P PORT=Enter PORT:
 ".\platform-tools\adb.exe" kill-server
 ".\platform-tools\adb.exe" connect localhost:%PORT%
 cd .\
-pip install --upgrade pip
-pip install Pillow==8.4.0
-pip install opencv-python==4.6.0.66
-pip install -r requirements.txt
+REM pip install --upgrade pip
+REM pip install Pillow==8.4.0
+REM pip install opencv-python==4.6.0.66
+REM pip install -r requirements.txt
 python -W ignore roktracker.py
 PAUSE
